9364df88d7c67011805c58beebe63453b636aa9b
deinesour <deineslelani5@gmail.com>
2024-03-23 16:15:34 -0600

    Beginning of events page

--- app/src/main/java/com/example/totw/eventsFragment.kt
+++ app/src/main/java/com/example/totw/eventsFragment.kt
@@ -1,34 +1,26 @@
+
 package com.example.totw
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import java.text.DateFormatSymbols
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [eventsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class eventsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private lateinit var calendarView: CalendarView
+    private lateinit var selectedDateText: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,24 +28,24 @@ class eventsFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_events, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        selectedDateText = view.findViewById(R.id.selectedDateText)
+        calendarView = view.findViewById(R.id.calendarView)
+        val currentDate = Calendar.getInstance().time
+        val dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.getDefault())
+        val dateString = dateFormat.format(currentDate)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EventsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            eventsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        selectedDateText.text = dateString
+
+        calendarView.setOnDateChangeListener{ view, year, month, dayOfMonth ->
+            val selected = Calendar.getInstance()
+            selected.set(year, month, dayOfMonth)
+            val monthName = DateFormatSymbols().months[month]
+            val selectedDate = "$monthName $dayOfMonth, $year"
+            selectedDateText.text = selectedDate
+
+        }
     }
+
 }
