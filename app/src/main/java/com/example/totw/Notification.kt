44d85d22bb623dc154c5514d7c980833e9b2e967
deinesour <deineslelani5@gmail.com>
2024-04-25 11:44:50 -0600

    NOTIFICATIONS!!!

--- app/src/main/java/com/example/totw/Notification.kt
+++ app/src/main/java/com/example/totw/Notification.kt
@@ -1,63 +1,24 @@
 package com.example.totw
 
-import android.Manifest
-import android.app.AlarmManager
-import android.app.PendingIntent
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import java.util.Calendar
-
+const val notificationID = 121
 class Notification : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
-        val notification = NotificationCompat.Builder(context!!, "channelId")
-            .setSmallIcon(R.drawable.logo_new)
+    override fun onReceive(context: Context, intent: Intent) {
+        // Show the notification
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val notification = NotificationCompat.Builder(context, channelID)
             .setContentTitle("Top O' The World")
             .setContentText("Check out our newest articles!")
+            .setSmallIcon(R.drawable.logo_new)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .build()
 
-        val notificationManager = NotificationManagerCompat.from(context)
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-            return
-        }
-        notificationManager.notify(1, notification)
-    }
-    companion object {
-        fun scheduleNotification(context: Context) {
-            try {
-                val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-                val intent = Intent(context, Notification::class.java)
-                val pendingIntent = PendingIntent.getBroadcast(
-                    context,
-                    0,
-                    intent,
-                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
-                )
-                val calendar = Calendar.getInstance().apply {
-                    timeInMillis = System.currentTimeMillis()
-                    set(Calendar.DAY_OF_WEEK, Calendar.TUESDAY)
-                    set(Calendar.HOUR_OF_DAY, 11)
-                    set(Calendar.MINUTE, 40)
-                    set(Calendar.SECOND, 0)
-                    set(Calendar.MILLISECOND, 0)
-                    if (timeInMillis <= System.currentTimeMillis()) {
-                        // If it's already Monday, schedule for the next Monday
-                        add(Calendar.WEEK_OF_YEAR, 1)
-                    }
-                }
-                    alarmManager.setExactAndAllowWhileIdle(
-                        AlarmManager.RTC_WAKEUP,
-                        calendar.timeInMillis,
-                        pendingIntent
-                    )
-            } catch (e: SecurityException) {
-                // App cannot schedule exact alarms, use inexact timing
-            }
-        }
+        notificationManager.notify(notificationID, notification)
     }
 }
--- app/src/main/java/com/example/totw/NotificationsFragment.kt
+++ app/src/main/java/com/example/totw/NotificationsFragment.kt
@@ -1,22 +1,32 @@
 package com.example.totw
 
+import android.annotation.SuppressLint
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Switch
 import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-
+import java.util.Calendar
+const val channelID = "channel1"
 class NotificationsFragment : Fragment() {
     private lateinit var notifSwitch: Switch
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var sharedViewModel: SharedViewModel
-
+    private lateinit var alarmManager: AlarmManager
+    private lateinit var notificationManager: NotificationManager
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,11 +48,6 @@ class NotificationsFragment : Fragment() {
 
         notifSwitch.setOnCheckedChangeListener{_, isChecked ->
             sharedPreferences.edit().putBoolean("switchState", isChecked).apply()
-            if(isChecked)
-                Notification.scheduleNotification(requireContext())
-            else{
-
-            }
         }
         generalSwitch.setOnCheckedChangeListener { _, isChecked ->
             sharedViewModel.setSwitchState("general", isChecked)
@@ -56,8 +61,91 @@ class NotificationsFragment : Fragment() {
         sportsSwitch.setOnCheckedChangeListener { _, isChecked ->
             sharedViewModel.setSwitchState("sports", isChecked)
         }
+
         return view
     }
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        // Initialize AlarmManager and NotificationManager
+        alarmManager = requireContext().getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        notificationManager =
+            requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Create the notification channel
+        createNotificationChannel()
+
+        if (notifSwitch.isChecked){
+            if (checkNotificationPermissions(requireContext())) {
+                // Schedule a notification
+                scheduleNotification()
+            }
+        }
+
+    }
+    fun checkNotificationPermissions(context: Context): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val isEnabled = notificationManager.areNotificationsEnabled()
+            if (!isEnabled) {
+                // Open the app notification settings if notifications are not enabled
+                val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+                intent.putExtra(Settings.EXTRA_APP_PACKAGE, requireContext().packageName)
+                requireContext().startActivity(intent)
+                return false
+            }
+        } else {
+                val areEnabled =
+                    NotificationManagerCompat.from(requireContext()).areNotificationsEnabled()
+
+                if (!areEnabled) {
+                    // Open the app notification settings if notifications are not enabled
+                    val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+                    intent.putExtra(Settings.EXTRA_APP_PACKAGE, requireContext().packageName)
+                    requireContext().startActivity(intent)
+                    return false
+                }
+            }
+        return true
+    }
+        @RequiresApi(Build.VERSION_CODES.O)
+        private fun createNotificationChannel() {
+            val name = "General"
+            val descriptionText = "General notifications from TOP."
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelID, name, importance).apply {
+                description = descriptionText
+            }
+            notificationManager.createNotificationChannel(channel)
+        }
+
+    @SuppressLint("ScheduleExactAlarm")
+    private fun scheduleNotification() {
+
+        val calendar = Calendar.getInstance().apply {
+            timeInMillis = System.currentTimeMillis()
+            set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY)
+            set(Calendar.HOUR_OF_DAY, 9)
+            set(Calendar.MINUTE,0)
+            set(Calendar.SECOND, 0)
+        }
+
+        val intent = Intent(requireContext(), Notification::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(
+            requireContext(),
+            notificationID,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        alarmManager.setExactAndAllowWhileIdle(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis,
+            pendingIntent
+        )
+    }
 }
+
+
+
 
